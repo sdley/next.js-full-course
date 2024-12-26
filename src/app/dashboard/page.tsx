06b25088b1889c1341166c0b4831f536f2e8af69
Souleymane DIALLO <sdley2020@gmail.com>
2024-12-26 21:40:09 +0000

    File colocation & routing file access

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function Barchart() {
+    return <h1>Bar chart</h1>;
+}
+
+export default function Dashboard() {
+    return <h1>Dashboard</h1>;
+}
