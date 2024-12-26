e11a3888fa97dcb2896a266491cd4e20c4de114c
Souleymane DIALLO <sdley2020@gmail.com>
2024-12-26 22:27:07 +0000

    Private Folders

---
+++ src/app/%5Flib/page.tsx
@@ -0,0 +1,8 @@
+export default function PrivateRoute() {
+    return (
+        <>
+            <h1>You can now view this in the browser because you used %5F</h1>
+            <p>%5F is the URL-encoded form of an underscore</p>
+        </>
+    );
+}
