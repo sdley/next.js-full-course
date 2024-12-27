7cb5bf4eacb157fc449b4c2b17dd7ef0cbd6fc06
Souleymane DIALLO <sdley2020@gmail.com>
2024-12-27 15:18:00 +0000

    Routing Metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,31 @@
+import { Metadata } from "next";
+
+type Props = {
+    params: {
+        productId: string
+    };
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+    return {
+        title: `Product ${params.productId}`,
+    };
+};
+
+// second method
+/*
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+    const title = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`iPhone ${params.productId}`)
+        }, 100)
+    })
+    return {
+        title: `Product ${title}`,
+    };
+}
+*/
+
 export default function ProductDetails({
     params
 }: {
