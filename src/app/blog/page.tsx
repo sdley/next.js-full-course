5562a1b303bb2118b86616e4b4e5ae2621a98af8
Souleymane DIALLO <sdley2020@gmail.com>
2024-12-27 15:41:44 +0000

    title Metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: {
+        absolute: "sdley Blog"
+    }
+};
+
 export default function Blog() {
     return <h1>My blog</h1>;
 }
