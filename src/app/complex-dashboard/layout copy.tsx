c5f67201f166e26b3f198357325fabf77961dcc1
Souleymane DIALLO <sdley2020@gmail.com>
2024-12-28 12:22:17 +0000

    Parralel Routes - Slots

---
+++ src/app/complex-dashboard/layout copy.tsx
@@ -0,0 +1,18 @@
+// import UserAnalytics from "@/components/UserAnalytics";
+// import RevenueMetrics from "@/components/RevenueMetrics";
+// import Notifications from "@/components/Notifications";
+
+// export default function DashboardLayout({
+//     children,
+// }: {
+//     children: React.ReactNode;
+// }) {
+//     return (
+//         <>
+//             <div>{children}</div> {/* Content from page.tsx */}
+//             <UserAnalytics /> {/* Component for user analytics */}
+//             <RevenueMetrics /> {/* Component for revenue metrics */}
+//             <Notifications /> {/* Component for notifications */}
+//         </>
+//     );
+// }
