2302d1b2a37a6965dd8e7c08054b98db375943de
Souleymane DIALLO <sdley2020@gmail.com>
2024-12-28 13:22:32 +0000

    Conditionnal Routes

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -2,14 +2,17 @@ export default function DashboardLayout({
     children,
     users,
     revenue,
-    notifications
+    notifications,
+    login
 }: {
     children: React.ReactNode;
     users: React.ReactNode;
     revenue: React.ReactNode;
     notifications: React.ReactNode;
+    login: React.ReactNode;
 }) {
-    return (
+    const isLoggedIn = true;
+    return isLoggedIn ? (
         <div>
             <div>{children}</div> {/* Content from page.tsx. This is equivalent to complex-dashboard/@children/pages.tsx*/}
             <div style={{display: "flex"}}>
@@ -20,5 +23,7 @@ export default function DashboardLayout({
                     <div style={{display: "flex", flex:1}}>{notifications}</div>
             </div>
         </div>
+    ) : (
+        login
     );
 }
